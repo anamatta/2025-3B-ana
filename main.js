ebac6c389d95565c168cc75d4a45754b6d2af0d6
Anaaa <ana.dornelles.matta@escola.pr.gov.br>
2025-07-03 10:34:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector("texto-resultado");
 
 const perguntas = [
     {
         enunciado: "Pergunta 1",
         alternativas: [
-            {
-                texto: "alternativa 01",
-                afirmacao: "resultado 01"
-            },
-            {
-                texto: "alternativa 02",
-                afirmacao: "resultado 02"
-            }
+           {
+            texto: "alternativa 01",
+            afirmacao: "resultado 01"
+           },
+           {
+            texto: "alternativa 02",
+            afirmacao: "resultado 02"
+           }
+
         ]
     },
     {
@@ -24,57 +25,60 @@ const perguntas = [
             {
                 texto: "alternativa 03",
                 afirmacao: "resultado 03"
-            },
-            {
+               },
+               {
                 texto: "alternativa 04",
                 afirmacao: "resultado 04"
-            }
+               }
+
         ]
-    },
-    {
+    }, {
         enunciado: "Pergunta 3",
         alternativas: [
             {
                 texto: "alternativa 05",
                 afirmacao: "resultado 05"
-            },
-            {
+               },
+               {
                 texto: "alternativa 06",
                 afirmacao: "resultado 06"
-            }
+               }
+
         ]
-    },
-    {
+    }, {
         enunciado: "Pergunta 4",
         alternativas: [
             {
                 texto: "alternativa 07",
                 afirmacao: "resultado 07"
-            },
-            {
+               },
+               {
                 texto: "alternativa 08",
                 afirmacao: "resultado 08"
-            }
+               }
+
         ]
     }
 ]
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal = "";
+let historiaFinal = " ";
 
-function mostraPergunta(){
+
+function mostraPergunta() {
     perguntaAtual = perguntas[atual]
     caixaPerguntas.textContent = perguntaAtual.enunciado
     mostraAlternativas()
 }
 
-function mostraAlternativas(){
-    for( const alternativa of perguntaAtual.alternativas ){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativa = document.createElement("button");
         botaoAlternativa.textContent = alternativa.texto;
-        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativa);
+        botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa))
+        caixaAlternativas.appendChild(botaoAlternativa)
+
     }
 }
 
